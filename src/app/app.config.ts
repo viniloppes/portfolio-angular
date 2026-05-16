b301d8fd62aa9c417451e266fe58cdda730e6a3d
Vinicius Lopes <viniloppes2000@gmail.com>
2026-05-15 22:19:14 -0300

    desabilitando ssr

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,7 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeuix/themes/aura';
 import { ButtonModule } from 'primeng/button';
@@ -112,7 +112,7 @@ export const appConfig: ApplicationConfig = {
 
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes), provideClientHydration(withEventReplay()),
+    provideRouter(routes),
     providePrimeNG({
       theme: {
         preset: Aura,
